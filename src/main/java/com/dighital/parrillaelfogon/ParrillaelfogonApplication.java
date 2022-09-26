2d43116e90b880a33b76423f3d1c0e55d4317177
Dighitalsoluciones <hernanglomba@gmail.com>
2022-09-26 15:47:30 -0300

    subida inicial

---
+++ src/main/java/com/dighital/parrillaelfogon/ParrillaelfogonApplication.java
@@ -0,0 +1,13 @@
+package com.dighital.parrillaelfogon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrillaelfogonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrillaelfogonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dighital/parrillaelfogon/ParrillaelfogonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dighital.parrillaelfogon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParrillaelfogonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
