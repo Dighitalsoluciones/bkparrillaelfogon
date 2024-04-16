b4cb512c2e6aa1bee79eb77e77dde2444fd72629
Dighitalsoluciones <hernanglomba@gmail.com>
2024-04-15 22:02:12 -0300

    rutas de produccion mesas controller y ticket controller

--- src/main/java/com/dighital/parrillaelfogon/controller/Mesas1Controller.java
+++ src/main/java/com/dighital/parrillaelfogon/controller/Mesas1Controller.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mesas1")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://parrielfogon.web.app")
 public class Mesas1Controller {
     
     @Autowired()
